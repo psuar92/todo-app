a8a03b54c253e9ef5ac2880bd0196594934ba850
Pedro <psuar92@gmail.com>
2024-09-13 20:46:43 -0500

    first commit

---
+++ src/store/todo.store.js
@@ -0,0 +1,82 @@
+import { Todo } from '../todos/models/todo';
+
+const Filters = {
+    All: 'all',
+    Completed: 'Completed',
+    Pending: 'Pending',
+}
+
+const state = {
+    todos: [
+        new Todo('Piedra del alma'),
+        new Todo('Piedra del infinito'),
+        new Todo('Piedra del tiempo'),
+    ],
+    filter: Filters.All,
+}
+
+const initStore = () => {
+    console.log(state);
+    console.log('InitStore');
+}
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = ( filter = Filters.All ) => {
+
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+
+}
+
+const addTodo = ( description ) => {
+    
+    if ( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo(description) );
+}
+
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+}
