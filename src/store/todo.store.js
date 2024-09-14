3fa99818eb71f05d954114a708927656809c4117
Pedro <psuar92@gmail.com>
2024-09-14 17:00:01 -0500

    first commit

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,28 +1,34 @@
-import { Todo } from '../todos/models/todo';
+import { Todo } from '../todos/models/todo-model';
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
-    Pending: 'Pending',
+    Pending: 'Pending'
 }
 
 const state = {
     todos: [
-        new Todo('Piedra del alma'),
-        new Todo('Piedra del infinito'),
-        new Todo('Piedra del tiempo'),
+        // Input value
     ],
     filter: Filters.All,
 }
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('InitStore');
 }
 
 const loadStore = () => {
-    throw new Error('Not implemented');
+    if ( !localStorage.getItem('state') ) return;
+
+    const { todos = [], filter = Filters.All } = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
 }
+
+const saveStateToLocalStorage = () => {
+    localStorage.setItem('state', JSON.stringify(state));
+};
 
 const getTodos = ( filter = Filters.All ) => {
 
@@ -47,6 +53,8 @@ const addTodo = ( description ) => {
     if ( !description ) throw new Error('Description is required');
 
     state.todos.push( new Todo(description) );
+
+    saveStateToLocalStorage();
 }
 
 const toggleTodo = ( todoId ) => {
@@ -56,18 +64,23 @@ const toggleTodo = ( todoId ) => {
         }
         return todo;
     });
+
+    saveStateToLocalStorage();
 }
 
 const deleteTodo = ( todoId ) => {
     state.todos = state.todos.filter( todo => todo.id !== todoId );
+    saveStateToLocalStorage();
 }
 
 const deleteCompleted = () => {
-    state.todos = state.todos.filter( todo => todo.done );
+    state.todos = state.todos.filter( todo => !todo.done );
+    saveStateToLocalStorage();
 }
 
 const setFilter = ( newFilter = Filters.All ) => {
     state.filter = newFilter;
+    saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () => {
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,18 @@
+import { createTodoHTML } from './';
+
+let element;
+
+export const renderTodos = ( elementId, todos = [] ) => {
+    if ( !element ) element = document.querySelector( elementId );
+
+    if ( !element ) throw new Error(`Element ${ elementId } not found`);
+
+    element.innerHTML = '';
+
+    todos.forEach( todo => {
+        element.append(createTodoHTML(todo));
+    });
+    
+
+};
+
